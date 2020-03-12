9d967226df8f117c040136afe3735b87d84c6cc0
kev <45981465+abbo-1@users.noreply.github.com>
2020-03-12 16:34:59 -0500

    sign in page done

--- src/components/SignUpSide.js
+++ src/components/SignUpSide.js
@@ -3,26 +3,25 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
+// import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+// import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import LocalBarIcon from '@material-ui/icons/LocalBar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-import backgroundImg from "../images/signInPhoto.png"
-
-
+// import Logo from '../images/tbMainLogoWood.png'
 
 const useStyles = makeStyles(theme => ({
   root: {
     height: '100vh',
   },
   image: {
-    // backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundImage: {backgroundImg},
+    backgroundImage: 'url(https://i.imgur.com/4rHxD9L.png)',
+    // backgroundImage: 'url(https://source.unsplash.com/Zrp9b3PMIy8/1416x1625)',
     backgroundRepeat: 'no-repeat',
     backgroundColor:
       theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[50],
@@ -40,13 +39,71 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: '100%',
     marginTop: theme.spacing(1),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
 }));
+
+// TEXT ANIMATION
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
+// END TEXT ANIMATION
 
 export default function SignInSide() {
   const classes = useStyles();
@@ -57,11 +114,17 @@ export default function SignInSide() {
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
-          <p>Remember that drink you had that one time? You remember, right? It was called...something with s? or a r? Oh forget it!</p>
+          <p class = "signInText">Remember that <span
+            class="txt-rotate"
+            data-period="2000"
+            data-rotate='[ "bottle of wine", "cocktail", "stout", "brand of whiskey"]'>
+          </span> you tried that one time? <br/>You remember, right? It was called something with s? It had a red label...or was it green?</p>   
+            <br />
+            <p class = "signInText">With Tasting Board you can log, track, and rate all the drinks you'll never forget again!</p>
           <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
+            <LocalBarIcon />
           </Avatar>
-          <Typography component="h1" variant="h5">
+          <Typography component="h1" variant="h6">
             Sign Up
           </Typography>
           <form className={classes.form} noValidate>

dad794ec7fb386414553ebb7b6b581f1c771aa95
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-03-21 10:21:27 -0500

    changes to merge

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 export default class Header extends Component {
   render() {
     return (
-      <div>
-        <nav className='navbar'></nav>
+      <div className='header'>
+       link
       </div>
     )
   }

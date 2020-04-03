d7c61664ea560f5d45231720e1024123731339d2
kev <45981465+abbo-1@users.noreply.github.com>
2020-04-02 20:17:38 -0500

    rearranged some things

--- src/components/Render.js
+++ src/components/Render.js
@@ -5,52 +5,72 @@ import axios from 'axios';
 import Card from './Card.js'
 
 class Render  extends React.Component {
-    constructor(props) {
+
+  constructor(props) {
         super(props);
         console.log("Here are your props", props);
+  }
 
- // call the drinks endpoint (via axios)
-  let options = {
-    method: 'GET',
-    url: 'http://localhost:8080/drinks',
-};
+componentDidMount() {
+  
+      // call the drinks endpoint (via axios)
+      let options = {
+        method: 'GET',
+        url: 'http://localhost:8080/drinks',
+      };
 
- // let drinks = array of drinks
-let drinksFromDatabase =  [];
-
-// axios request
-   axios.request(options)
-     .then(function (response) {
-      // console.log(response);
-      drinksFromDatabase =  [response.data]
-        console.log("look here")
-        console.log(drinksFromDatabase)
+      // let drinks = array of drinks
+      let drinksFromDatabase =  [];
+      console.log("this is",this);
+    // axios request
+      axios.request(options)
+        .then( (response) => {
+          console.log(response);
+          drinksFromDatabase =  response.data
+          console.log("look here")
+          console.log(drinksFromDatabase)
+          console.log("this is",this);
+          this.setState(drinksFromDatabase)
     })
-     .catch(function (error) {
+    .catch(function (error) {
       console.log(error);
     });
-  };
 }
 
 // let cardComponents = // array of Card (Card.js)
-let cardComponents = [{ Card }]
+// let cardComponents = [{ Card }]
 
-  // let cardComponents = []
-let cardComponents = []
 
    // loop through array of drinks
+
+
+
           // add to cardComponents
 
-render() {
+  render() {
 
+     let drinks = this.state;
+  console.log("between this")
+  console.log(drinks);
+  console.log("between and this")
 
+    // let selectedDrinks = drinks.map(
+    //   function() {
+    
+    //     // cardComponents.push()
+    //   }
+    // )
 
-    // cardComponents has all the components to render
+  // let cardComponents = []
+  //let cardComponents = []
+
+  // cardComponents has all the components to render
+
 
       let drinkChoice = this.props.reduxState.drinkChoice
          if (drinkChoice === undefined) {
              drinkChoice = "all"
-             callDrinks
+            //  callDrinks
          }
          let information = []
          console.log("Drink Choice is", drinkChoice)
@@ -76,10 +96,13 @@ render() {
         default :
         }
 
-        return (
-            <div>{information}</div>
-            // <div> {cardComponents} </div>
-        )
+  return (
+    <div>hi</div>
+    //<div>{information}</div>
+     //<div> {cardComponents} </div>
+    )
+  }
+
 }
 
 // export default connect()(Render);

d32db1cb12b94ecd97b11454e7e63bc2e7ec320b
kev <45981465+abbo-1@users.noreply.github.com>
2020-04-27 17:44:32 -0500

    organiztion

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
     name: DataTypes.STRING,
--- src/components/SignUpSide.js
+++ src/components/SignUpSide.js
@@ -188,12 +188,6 @@ export default function SignInSide() {
                   autoComplete='current-password'
                 />
               </Grid>
-              {/* <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration and updates via email."
-              />
-            </Grid> */}
             </Grid>
             <Button
               type='submit'
